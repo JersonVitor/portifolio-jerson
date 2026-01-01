5afac1c0fa490e3d61939ba8aaf9b43361dd5e75
JersonVitor <jersonitor2016@gmail.com>
2025-12-31 21:07:20 -0300

    feat: deploy to github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  // basePath: "/repo-name", // Uncomment and set if deploying to username.github.io/repo-name
 };
-
 export default nextConfig;
